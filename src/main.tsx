4290da9e8887d18687478f72a77a8fcee769a8b6
yubo2426-webzine <yubo2426@gmail.com>
2026-06-25 17:31:59 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,9 @@ import 'krds-react/dist/index.css'
 // 🚨 .jsx 확장자 제거
 import App from './App'
 
-// 💡 개발자 이스터에그: F12(개발자 도구) 콘솔창에서만 보이는 은밀한 발자취입니다.
+// 💡 F12(개발자 도구) 콘솔창을 열어보세요
 console.log(
-  "%c🚀 함께누리웹진\n%c2026년 당시 유초특과 이두호로부터 시작", 
+  "%c🚀 함께누리웹진\n%c2026년 당시 유초특과 이두호로부터 시작(2026.6.25.)", 
   "font-size: 20px; font-weight: bold; color: #10b981;", 
   "font-size: 13px; color: #64748b; font-style: italic; margin-top: 4px;"
 );
